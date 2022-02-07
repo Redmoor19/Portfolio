6a41f975856719f5f12317443f7b72bab97ccbdf
Redmoor19 <kald03@yandex.ru>
2022-02-07 13:54:36 +0200

    Skills page

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,10 @@ function toggleSidebar () {
     const hamburger = document.getElementsByClassName('hamburger')[0];
     hamburger.classList.toggle('hamburger__deactivate');
 }
+
+const counters = document.querySelectorAll('.skills__graphs_persentage'),
+         lines = document.querySelectorAll('.skills__graphs-item-body-sausage');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+})
